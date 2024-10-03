70e509f8f752f3fb00ffb953c27024e8f2ddabcc
Максим <maximers6e@gmail.com>
2024-10-03 10:07:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 correct_password = "1234"
-user_password = input("Введите пароль: ")
+user_password = input("Введите пароль: ") #ввод переменной
 if user_password == correct_password:
     print("Доступ разрешен")
 else:
